7ad3bb1c8a3d5fe38fca6dc09885bb3979867fbf
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2024-04-29 12:52:52 +0800

    created toggle example component

---
+++ src/tutorial/CustomHooks/SampleUseCase/ToggleExample.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import useToggle from "../useToggle";
+
+const ToggleExample = () => {
+  const { show, toggle } = useToggle(true);
+
+  return (
+    <div>
+      <h4>Toggle Custom Hook</h4>
+      <button className="btn" onClick={toggle}>
+        Toggle
+      </button>
+      {show && <h4>Some Stuff</h4>}
+    </div>
+  );
+};
+
+export default ToggleExample;
