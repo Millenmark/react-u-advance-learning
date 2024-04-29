811c74ba46839252011093623e748bf32aa54f7d
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2024-04-29 12:51:45 +0800

    added some components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import UseStateTuts from "./tutorial/useState/UseStateTuts";
 import UseEffectTuts from "./tutorial/useEffect/UseEffectTuts";
 import ConditionalRendering from "./tutorial/useEffect/ConditionalRendering";
 import CleanUpFunc from "./tutorial/useEffect/CleanUpFunc";
+import ControlledInputs from "./tutorial/Forms/ControlledInputs";
+import UseRefTuts from "./tutorial/useRef/UseRefTuts";
+import ToggleExample from "./tutorial/CustomHooks/SampleUseCase/ToggleExample";
+import Navbar from "./tutorial/ContextAPI/Navbar";
+import UseReducerTuts from "./tutorial/useReducer/UseReducerTuts";
 
 function App() {
   return (
@@ -10,7 +15,12 @@ function App() {
       {/* <UseStateTuts /> */}
       {/* <UseEffectTuts /> */}
       {/* <ConditionalRendering /> */}
-      <CleanUpFunc />
+      {/* <CleanUpFunc /> */}
+      {/* <ControlledInputs /> */}
+      {/* <UseRefTuts /> */}
+      {/* <ToggleExample /> */}
+      {/* <Navbar /> */}
+      <UseReducerTuts />
     </div>
   );
 }
