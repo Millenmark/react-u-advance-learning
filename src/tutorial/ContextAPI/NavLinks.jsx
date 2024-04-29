5d9f7d60d752206451ab89b1ae8f43672cfb9ad2
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2024-04-29 12:52:10 +0800

    created navlink component

---
+++ src/tutorial/ContextAPI/NavLinks.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import UserContainer from "./UserContainer";
+
+const NavLinks = () => {
+  return (
+    <div className="nav-container">
+      <ul className="nav-links">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+      </ul>
+      <UserContainer />
+    </div>
+  );
+};
+
+export default NavLinks;
